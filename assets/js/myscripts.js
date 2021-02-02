b5139d5dfd6fb6c0e5843b0d50cc0fac2d968846
Abram M. Unabia <unabiabram@gmail.com>
2021-02-03 06:05:18 +0800

    pwa

--- assets/js/myscripts.js
+++ assets/js/myscripts.js
@@ -1,4 +1,9 @@
-if ("serviceWorker" in navigator) {
-  // register service worker
-  navigator.serviceWorker.register("service-worker.js");
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/service-worker.js')
+  .then(function(registration) {
+    console.log('Registration successful, scope is:', registration.scope);
+  })
+  .catch(function(error) {
+    console.log('Service worker registration failed, error:', error);
+  });
 }
